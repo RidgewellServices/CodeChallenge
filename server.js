50809c5f85cbc9d326c53d53b20477222ca21918
Ramona Ridgewell <ramona@ridgewellservices.com>
2016-12-06 10:47:49 -0800

    trying to get heroku to serve page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+var http = require('http')
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/plain'});
+ res.send('it is running\n');
+}).listen(process.env.PORT || 5000);
